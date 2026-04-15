e1cbddfb2b096894e655c3f96021c5c027fcc8ef
Anuj-12 <anujtjs@gmail.com>
2026-04-15 23:17:04 +0530

    Shortcut comments

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,8 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {}
+    -- Normal Mode
+    -- gcc: comment a line, gbc: comment in blocks
+    -- Visual Mode
+    -- gc and gb
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -77,6 +77,7 @@
 
           -- References
           map('grr', vim.lsp.buf.references, '[G]oto [R]eferences')
+          map('K', vim.lsp.buf.hover, "Hover docs")
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
